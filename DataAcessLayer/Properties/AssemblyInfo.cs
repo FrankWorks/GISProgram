22ce24ea55f6bcbccbfd81e36b3cdb41765cfa5a
Frank Kim <fkim@parks.lacounty.gov>
2018-09-21 14:01:30 -0700

    Added Version Label on the Landing Page

--- DataAcessLayer/Properties/AssemblyInfo.cs
+++ DataAcessLayer/Properties/AssemblyInfo.cs
@@ -2,34 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GISProgram")]
+[assembly: AssemblyTitle("DataAcessLayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Frank Kim")]
-[assembly: AssemblyProduct("GISProgram")]
-[assembly: AssemblyCopyright("Copyright © Frank Kim 2018")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("DataAcessLayer")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9b5a9a05-c3b9-4489-84d1-eea6920bebbd")]
+[assembly: Guid("b568a019-34f9-44c1-9eca-1398e91d47d2")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.*")]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
