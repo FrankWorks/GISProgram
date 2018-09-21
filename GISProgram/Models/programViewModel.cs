d6d981b40e7180ba69854ee67bd48fbf68b1c08f
Frank Kim <fkim@parks.lacounty.gov>
2018-09-21 01:38:55 -0700

    Program Add / Edit Field Validator Redirect from Home View to Grid1 View

--- GISProgram/Models/programViewModel.cs
+++ GISProgram/Models/programViewModel.cs
@@ -9,21 +9,34 @@ namespace GISProgram.Models
 {
     public class programViewModel
     {
+        [Required]
         public string name { get; set; }
+        [Required]
         public string parkName { get; set; }
+        [Required]
         public long locationID { get; set; }
         [Key]
         public Nullable<int> programID { get; set; }
+        [Required]
         public string programName { get; set; }
         public string type { get; set; }
+        [Required]
         public string description { get; set; }
+        [Required]
         public string displayAge { get; set; }
+        [Required]
         public string feeStructure { get; set; }
+        [Required]
         public Nullable<bool> registrationRequired { get; set; }
+        [Required]
         public string registrationFee { get; set; }
+        [Required]
         public string frequencyType { get; set; }
+        [Required]
         public string frequency { get; set; }
+
         public string specialCriteria { get; set; }
+        [Required]
         public string programCategoryName { get; set; }
     }
 }
